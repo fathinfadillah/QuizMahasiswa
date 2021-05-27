b45e86a0e0896486e6034684646f9bf46dce959d
Muhammad Fathin Fadillah <0320200016@polman.astra.ac.id; m.fathinfadillah@gmail.com>
2021-05-27 09:45:20 +0700

    benerin form report

--- QuizMahasiswa/ReportBarang014.Designer.cs
+++ QuizMahasiswa/ReportBarang014.Designer.cs
@@ -31,7 +31,7 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.dt = new System.Windows.Forms.DataGridView();
 			this.btnExcel = new System.Windows.Forms.Button();
-			this.btnPdf = new System.Windows.Forms.Button();
+			this.btnPDF = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -50,7 +50,7 @@
 			// 
 			this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dt.Location = new System.Drawing.Point(56, 107);
-			this.dt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.dt.Margin = new System.Windows.Forms.Padding(2);
 			this.dt.Name = "dt";
 			this.dt.RowTemplate.Height = 24;
 			this.dt.Size = new System.Drawing.Size(582, 279);
@@ -59,34 +59,35 @@
 			// btnExcel
 			// 
 			this.btnExcel.Location = new System.Drawing.Point(220, 439);
-			this.btnExcel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnExcel.Margin = new System.Windows.Forms.Padding(2);
 			this.btnExcel.Name = "btnExcel";
 			this.btnExcel.Size = new System.Drawing.Size(82, 26);
 			this.btnExcel.TabIndex = 2;
 			this.btnExcel.Text = "Export Excel";
 			this.btnExcel.UseVisualStyleBackColor = true;
+			this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
 			// 
-			// btnPdf
+			// btnPDF
 			// 
-			this.btnPdf.Location = new System.Drawing.Point(392, 439);
-			this.btnPdf.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.btnPdf.Name = "btnPdf";
-			this.btnPdf.Size = new System.Drawing.Size(82, 26);
-			this.btnPdf.TabIndex = 2;
-			this.btnPdf.Text = "Export PDF";
-			this.btnPdf.UseVisualStyleBackColor = true;
-			this.btnPdf.Click += new System.EventHandler(this.btnPdf_Click);
+			this.btnPDF.Location = new System.Drawing.Point(392, 439);
+			this.btnPDF.Margin = new System.Windows.Forms.Padding(2);
+			this.btnPDF.Name = "btnPDF";
+			this.btnPDF.Size = new System.Drawing.Size(82, 26);
+			this.btnPDF.TabIndex = 2;
+			this.btnPDF.Text = "Export PDF";
+			this.btnPDF.UseVisualStyleBackColor = true;
+			this.btnPDF.Click += new System.EventHandler(this.btnPDF_Click);
 			// 
 			// ReportBarang014
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(694, 516);
-			this.Controls.Add(this.btnPdf);
+			this.Controls.Add(this.btnPDF);
 			this.Controls.Add(this.btnExcel);
 			this.Controls.Add(this.dt);
 			this.Controls.Add(this.label1);
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "ReportBarang014";
 			this.Text = "ReportBarabg";
 			((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
@@ -100,6 +101,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dt;
         private System.Windows.Forms.Button btnExcel;
-        private System.Windows.Forms.Button btnPdf;
+        private System.Windows.Forms.Button btnPDF;
     }
 }
