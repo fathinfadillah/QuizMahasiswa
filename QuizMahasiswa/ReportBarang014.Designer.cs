b9ea915381fe59a0b3f54df9cbe2ccf5467dfa3c
Muhammad Fathin Fadillah <0320200016@polman.astra.ac.id; m.fathinfadillah@gmail.com>
2021-05-27 09:34:28 +0700

    benerin form report barang

--- QuizMahasiswa/ReportBarang014.Designer.cs
+++ QuizMahasiswa/ReportBarang014.Designer.cs
@@ -28,65 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.dt = new System.Windows.Forms.DataGridView();
-            this.btnExcel = new System.Windows.Forms.Button();
-            this.btnPdf = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(257, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(229, 38);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Report Barang";
-            // 
-            // dt
-            // 
-            this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dt.Location = new System.Drawing.Point(12, 95);
-            this.dt.Name = "dt";
-            this.dt.RowTemplate.Height = 24;
-            this.dt.Size = new System.Drawing.Size(776, 243);
-            this.dt.TabIndex = 1;
-            // 
-            // btnExcel
-            // 
-            this.btnExcel.Location = new System.Drawing.Point(173, 372);
-            this.btnExcel.Name = "btnExcel";
-            this.btnExcel.Size = new System.Drawing.Size(110, 32);
-            this.btnExcel.TabIndex = 2;
-            this.btnExcel.Text = "Export Excel";
-            this.btnExcel.UseVisualStyleBackColor = true;
-            // 
-            // btnPdf
-            // 
-            this.btnPdf.Location = new System.Drawing.Point(445, 372);
-            this.btnPdf.Name = "btnPdf";
-            this.btnPdf.Size = new System.Drawing.Size(110, 32);
-            this.btnPdf.TabIndex = 2;
-            this.btnPdf.Text = "Export PDF";
-            this.btnPdf.UseVisualStyleBackColor = true;
-            this.btnPdf.Click += new System.EventHandler(this.btnPdf_Click);
-            // 
-            // ReportBarang014
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnPdf);
-            this.Controls.Add(this.btnExcel);
-            this.Controls.Add(this.dt);
-            this.Controls.Add(this.label1);
-            this.Name = "ReportBarang014";
-            this.Text = "ReportBareng014";
-            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dt = new System.Windows.Forms.DataGridView();
+			this.btnExcel = new System.Windows.Forms.Button();
+			this.btnPdf = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Renogare", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(258, 40);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(179, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Report Barang";
+			// 
+			// dt
+			// 
+			this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dt.Location = new System.Drawing.Point(56, 107);
+			this.dt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.dt.Name = "dt";
+			this.dt.RowTemplate.Height = 24;
+			this.dt.Size = new System.Drawing.Size(582, 279);
+			this.dt.TabIndex = 1;
+			// 
+			// btnExcel
+			// 
+			this.btnExcel.Location = new System.Drawing.Point(220, 439);
+			this.btnExcel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnExcel.Name = "btnExcel";
+			this.btnExcel.Size = new System.Drawing.Size(82, 26);
+			this.btnExcel.TabIndex = 2;
+			this.btnExcel.Text = "Export Excel";
+			this.btnExcel.UseVisualStyleBackColor = true;
+			// 
+			// btnPdf
+			// 
+			this.btnPdf.Location = new System.Drawing.Point(392, 439);
+			this.btnPdf.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnPdf.Name = "btnPdf";
+			this.btnPdf.Size = new System.Drawing.Size(82, 26);
+			this.btnPdf.TabIndex = 2;
+			this.btnPdf.Text = "Export PDF";
+			this.btnPdf.UseVisualStyleBackColor = true;
+			this.btnPdf.Click += new System.EventHandler(this.btnPdf_Click);
+			// 
+			// ReportBarang014
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(694, 516);
+			this.Controls.Add(this.btnPdf);
+			this.Controls.Add(this.btnExcel);
+			this.Controls.Add(this.dt);
+			this.Controls.Add(this.label1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "ReportBarang014";
+			this.Text = "ReportBarabg";
+			((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
--- QuizMahasiswa/ReportBarang014.cs
+++ QuizMahasiswa/ReportBarang014.cs
@@ -20,48 +20,94 @@ namespace QuizMahasiswa
             InitializeComponent();
         }
 
-        private void btnPdf_Click(object sender, EventArgs e)
-        {
-            BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
-            PdfPTable pdftable = new PdfPTable(dt.Columns.Count);
-            pdftable.DefaultCell.Padding = 3;
-            pdftable.WidthPercentage = 100;
-            pdftable.HorizontalAlignment = Element.ALIGN_LEFT;
-            pdftable.DefaultCell.BorderWidth = 1;
+		public void exportgridtopdf(DataGridView dgw, string filename)
+		{
+			BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
+			PdfPTable pdftable = new PdfPTable(dgw.Columns.Count);
+			pdftable.DefaultCell.Padding = 3;
+			pdftable.WidthPercentage = 100;
+			pdftable.HorizontalAlignment = Element.ALIGN_LEFT;
+			pdftable.DefaultCell.BorderWidth = 1;
 
-            iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
-            //Add header
-            foreach (DataGridViewColumn column in dt.Columns)
+			iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
 
-            {
-                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, text));
-                cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
-                pdftable.AddCell(cell);
-            }
+			//Add header
+			foreach (DataGridViewColumn column in dgw.Columns)
+			{
+				PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, text));
+				cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+				pdftable.AddCell(cell);
+			}
+			//Add datarow
+			foreach (DataGridViewRow row in dgw.Rows)
+			{
+				foreach (DataGridViewCell cell in row.Cells)
+				{
+					pdftable.AddCell(new Phrase(cell.Value.ToString(), text));
+				}
+			}
 
-            foreach (DataGridViewRow row in dt.Rows)
-            {
-                foreach (DataGridViewCell cell in row.Cells)
-                {
-                    pdftable.AddCell(new Phrase(cell.Value.ToString(), text));
-                }
-            }
+			var savefiledialoge = new SaveFileDialog();
+			savefiledialoge.FileName = filename;
+			savefiledialoge.DefaultExt = ".pdf";
+			if (savefiledialoge.ShowDialog() == DialogResult.OK)
+			{
+				using (FileStream stream = new FileStream(savefiledialoge.FileName, FileMode.Create))
+				{
+					Document pdfdoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+					PdfWriter.GetInstance(pdfdoc, stream);
+					pdfdoc.Open();
+					pdfdoc.Add(pdftable);
+					pdfdoc.Close();
+					stream.Close();
+				}
+			}
+		}
 
-            var savefiledialoge = new SaveFileDialog();
-            savefiledialoge.FileName = filename;
-            savefiledialoge.DefaultExt = ".pdf";
-            if (savefiledialoge.ShowDialog() == DialogResult.OK)
-            {
-                using (FileStream stream = new FileStream(savefiledialoge.FileName, FileMode.Create))
-                {
-                    Document pdfdoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
-                    PdfWriter.GetInstance(pdfdoc, stream);
-                    pdfdoc.Open();
-                    pdfdoc.Add(pdftable);
-                    pdfdoc.Close();
-                    stream.Close();
-                }
-            }
-        }
-    }
+		public void exportgridtoexcel(DataGridView dgw, string filename)
+		{
+			Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+			Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+			Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+			worksheet = workbook.Sheets["Sheet1"];
+			worksheet = workbook.ActiveSheet;
+			worksheet.Name = "ProductDetail";
+
+			// storing header part in Excel 
+			for (int i = 1; i < dt.Columns.Count + 1; i++)
+			{
+				worksheet.Cells[1, i] = dt.Columns[i - 1].HeaderText;
+			}
+			// storing Each row and column value to excel sheet  
+			for (int i = 0; i < dt.Rows.Count; i++)
+			{
+				for (int j = 0; j < dt.Columns.Count; j++)
+				{
+					worksheet.Cells[i + 2, j + 1] = dt.Rows[i].Cells[j].Value.ToString();
+				}
+			}
+
+			//save excel
+			var saveFileDialogeExcel = new SaveFileDialog();
+			saveFileDialogeExcel.FileName = "output excel";
+			saveFileDialogeExcel.DefaultExt = ".xlsx";
+
+			if (saveFileDialogeExcel.ShowDialog() == DialogResult.OK)
+			{
+				workbook.SaveAs(saveFileDialogeExcel.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+					Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+			}
+			app.Quit();
+		}
+
+		private void btnPDF_Click(object sender, EventArgs e)
+		{
+			exportgridtopdf(dt, "test");
+		}
+
+		private void btnExcel_Click(object sender, EventArgs e)
+		{
+			exportgridtoexcel(dt, "test");
+		}
+	}
 }
